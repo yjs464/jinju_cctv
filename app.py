10cc01f5b5c1a1803a0b6f1bfd2d75073dfc079f
yjs464 <wall850@naver.com>
2025-06-09 04:03:44 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from streamlit_folium import folium_static
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
+
+st.title("진주시 CCTV 현황")
+
+df = pd.read_csv("jinju_cctv_20250513.csv", encoding='euc-kr')
+st.dataframe(df, height=200)
+df[["lat", "lon"]] = df[["위도", "경도"]]
+
+m = folium.Map(location=[35.1799817, 128.1076213], zoom_start=13)
+marker_cluster = MarkerCluster().add_to(m)
+
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row["lat"], row["lon"]],
+        popup=row["설치장소"],
+        icon=folium.Icon(color="blue", icon="info-sign")
+    ).add_to(marker_cluster)
+
+folium_static(m)
